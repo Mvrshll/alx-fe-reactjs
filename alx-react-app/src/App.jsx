86a5749cdde196300ec4ec7e700a95a98818a2f8
Mvrshll <michaelangelomarshall@gmail.com>
2024-08-11 01:45:14 +0100

    third commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -17,8 +20,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <WelcomeMessage />
       <h1>Vite + React</h1>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ alx-react-app/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent () {
+    return (
+        <main>
+            <p>I love to visit new York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
