c5a76961fe12106b61cffcff7763992a5ade7623
Mvrshll <michaelangelomarshall@gmail.com>
2024-08-19 00:03:13 +0100

    build company with react

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px' }}>
+    <nav style={{ display: 'flex', justifyContent: 'space-around', padding: '10px', backgroundColor: '#f0f0f0' }}>
       <Link to="/" style={{ textDecoration: 'none', color: '#333' }}>Home</Link>
       <Link to="/about" style={{ textDecoration: 'none', color: '#333' }}>About</Link>
       <Link to="/services" style={{ textDecoration: 'none', color: '#333' }}>Services</Link>
