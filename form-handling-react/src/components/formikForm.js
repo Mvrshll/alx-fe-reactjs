52103bd1cb14c280b1cd86f4c3ad15d52dc13a25
Mvrshll <michaelangelomarshall@gmail.com>
2024-08-31 03:26:14 +0100

    Implementing Form Handling in React with Controlled Components and Formik

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,50 +1,40 @@
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
-const RegistrationForm = () => {
-  const initialValues = {
-    username: '',
-    email: '',
-    password: '',
-  };
+const FormikForm = () => {
+  const initialValues = { username: '', email: '', password: '' };
 
   const validationSchema = Yup.object({
-    username: Yup.string()
-      .required('Username is required'),
-    email: Yup.string()
-      .email('Invalid email format')
-      .required('Email is required'),
-    password: Yup.string()
-      .required('Password is required'),
+    username: Yup.string().required('Username is required'),
+    email: Yup.string().email('Invalid email format').required('Email is required'),
+    password: Yup.string().required('Password is required'),
   });
 
-  const handleSubmit = (values) => {
-    // Simulate API call
-    console.log(values);
-    alert('Registration successful!');
+  const onSubmit = (values) => {
+    console.log('Form submitted:', values);
   };
 
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
-      onSubmit={handleSubmit}
+      onSubmit={onSubmit}
     >
       {() => (
         <Form>
           <div>
             <label>Username:</label>
-            <Field type="text" name="username" />
+            <Field name="username" type="text" />
             <ErrorMessage name="username" component="div" style={{ color: 'red' }} />
           </div>
           <div>
             <label>Email:</label>
-            <Field type="email" name="email" />
+            <Field name="email" type="email" />
             <ErrorMessage name="email" component="div" style={{ color: 'red' }} />
           </div>
           <div>
             <label>Password:</label>
-            <Field type="password" name="password" />
+            <Field name="password" type="password" />
             <ErrorMessage name="password" component="div" style={{ color: 'red' }} />
           </div>
           <button type="submit">Register</button>
@@ -54,4 +44,4 @@ const RegistrationForm = () => {
   );
 };
 
-export default RegistrationForm;
+export default FormikForm;
