b71e9d6d3c4a8ae9db383addc407217c43afd821
Mvrshll <michaelangelomarshall@gmail.com>
2024-09-14 02:22:01 +0100

    created a ned id int the data.json

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: class, // Enabled class-based dark mode
+  darkMode: 'class', // Enabled class-based dark mode
   theme: {
     extend: {
       // Custom theme settings
