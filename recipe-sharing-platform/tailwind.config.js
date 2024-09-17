bd19319e5f7e9c1619d9cec13edba058e3ac8827
Mvrshll <michaelangelomarshall@gmail.com>
2024-09-17 08:40:48 +0100

    updated the tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // For Tailwind CSS v2.x, you would use the purge option
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
+  // content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // Enabled class-based dark mode
   theme: {
     extend: {
-      // Custom theme settings
+      
     },
   },
   variants: {
