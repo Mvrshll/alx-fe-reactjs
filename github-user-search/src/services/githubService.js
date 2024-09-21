5573565403da19055f2098d68566086cab5ef79d
Mvrshll <michaelangelomarshall@gmail.com>
2024-09-21 14:54:46 +0100

    second commit

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,12 +2,17 @@ import axios from 'axios';
 
 const BASE_URL = 'https://api.github.com';
 
-export const fetchUserData = async (username) => {
+export const searchUsers = async (username) => {
   try {
-    const response = await axios.get(`${BASE_URL}/users/${username}`);
-    return response.data;
+    const response = await axios.get(`${BASE_URL}/search/users`, {
+      params: { q: query },
+      headers: {
+        Authorization: `token ${import.meta.env.VITE_GITHUB_API_KEY}`,
+      },
+    });
+    return response.data.items;
   } catch (error) {
-    console.error('Error fetching user data:', error);
-    throw error;
+    console.error('Error searching users:', error);
+    return [];
   }
 };
